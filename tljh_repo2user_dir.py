7f7a1e4d30cbc4bb802fdbff7544a6008bfdacb6
Naomi Alterman <naomila@uw.edu>
2025-05-19 15:07:04 -0700

    Add further configuration parameters

--- tljh_repo2user_dir.py
+++ tljh_repo2user_dir.py
@@ -5,12 +5,16 @@ from pwd import getpwnam
 from git import Repo
 from tljh.hooks import hookimpl
 
-def clone_repo(user, git_url, repo_dir):
+def clone_repo(user, git_url, branch_name, repo_dir):
     """
     A function to clone a github repo into a specific directory of a given user.
     User receiver owner rights of all files in the repository
     """
-    Repo.clone_from(git_url, repo_dir)
+    clone_options = []
+    if branch_name is not None:
+        clone_options.append([f"--branch {branch_name}"])
+
+    Repo.clone_from(git_url, repo_dir, multi_options=clone_options)
     uid = getpwnam(user).pw_uid
     gid = getpwnam(user).pw_gid
     for root, dirs, files in os.walk(repo_dir):
@@ -32,12 +36,18 @@ def tljh_new_user_create(username):
     # nothing to do if no repo is specified
     if git_url is None:
         return
-    
-    repo_dir = os.path.join(user_root_dir, 'repos')
+
+    branch_name = os.getenv("REPO_BRANCH")
+
+    repo_collection_subdir = os.getenv("REPO_DST")
+    if repo_collection_subdir is not None:
+        repo_dir = os.path.join(user_root_dir, repo_collection_subdir)
+    else:
+        repo_dir = os.path.join(user_root_dir, repo_collection_subdir)
 
     if not os.path.isdir(repo_dir):
         os.makedirs(repo_dir)
-        clone_repo(username, git_url, repo_dir)
+        clone_repo(username, git_url, branch_name, repo_dir)
     else:
         # user already has the repo downloaded
         pass
